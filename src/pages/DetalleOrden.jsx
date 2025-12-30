e6547b3054df1040924b001e4feab53e2bec5044
cryptodominicano <isaiasperez1977@gmail.com>
2025-12-30 00:08:23 -0500

    Fix: Increase bottom padding so photos aren't hidden by action buttons

--- src/pages/DetalleOrden.jsx
+++ src/pages/DetalleOrden.jsx
@@ -390,7 +390,7 @@ export default function DetalleOrden() {
   }
 
   return (
-    <div className="p-4 space-y-4 pb-24">
+    <div className="p-4 space-y-4 pb-56">
       {/* Header */}
       <div className="flex items-center gap-3">
         <Link to={createPageUrl('MisOrdenes')}>
